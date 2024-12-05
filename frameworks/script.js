69e91554826a095c78aa163717d0b1ad1c1b48f5
HarkovIndustry <xiiharkov@gmail.com>
2024-12-05 08:18:30 +0800

    Add files via upload

---
+++ frameworks/script.js
@@ -0,0 +1,323 @@
+const spot = [
+  {
+    path: "images/Alona Beach/",
+    title: "Alona Beach",
+    description:
+      "Relax on the stunning, kilometer-long white sand beach in Panglao Island, surrounded by crystal-clear waters and lush palm trees. Enjoy the serene ambiance, perfect for sunbathing, swimming, and snorkeling. Experience local cuisine at beachfront restaurants and immerse yourself in the natural beauty of this tropical paradise.",
+    address: {
+      location: "Panglao Island, Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6864.963267105474!2d123.76675494140645!3d9.548103712141632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33abac9f32e574a7%3A0x48e7cb35746858e1!2sAlona%20Beach!5e1!3m2!1sen!2sph!4v1732966307232!5m2!1sen!2sph",
+    },
+    fee: "Free",
+    contact: {
+      number: "+63 938 502 8101",
+      button: "Contact Location",
+    },
+    images: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"],
+  },
+  {
+    path: "images/Baclayon Church/",
+    title: "Baclayon Church",
+    description:
+      "Dedicated to Our Lady of the Immaculate Conception, this ancient Catholic stone church showcases Bohol's rich history. Marvel at its intricate architecture, which has stood the test of time since the 16th century. The church houses religious artifacts and offers a glimpse into the island's colonial past, making it a must-visit for history enthusiasts and devotees alike.",
+    address: {
+      location: "Main Road, Baclayon, Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3933.6792913652007!2d123.91245119999999!3d9.6228578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa4e3f6a91f0c3%3A0xe2eacd652008b449!2sBaclayon%20Church!5e0!3m2!1sen!2sph!4v1732967127375!5m2!1sen!2sph",
+    },
+    fee: "Free",
+    contact: {
+      number: "+63 938 540 9088",
+      button: "Contact Location",
+    },
+    images: [
+      "1.jpg",
+      "2.jpg",
+      "3.jpg",
+      "4.jpg",
+      "5.jpg",
+      "6.jpg",
+      "7.JPG",
+      "8.JPG",
+    ],
+  },
+  {
+    path: "images/Bilar Man-Made Forest/",
+    title: "Bilar Man-Made Forest",
+    description:
+      "Stroll through the refreshing, two-kilometer stretch of Mahogany trees that make up the Bilar Man-Made Forest. This lush green canopy provides a cool respite and a captivating visual experience, with towering trees that create a tunnel-like effect. Perfect for nature lovers and photographers, this serene forest is an ideal spot to connect with nature and capture stunning photos.",
+    address: {
+      location: "Loay Interior Road, Bilar, Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3933.1966496325167!2d124.07847400000001!3d9.6642323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa4158fa167357%3A0xef93d9624ddbd561!2sBilar%20Man-Made%20Forest!5e0!3m2!1sen!2sph!4v1732967248644!5m2!1sen!2sph",
+    },
+    fee: "Free",
+    contact: {
+      number: "+63 938 540 9088",
+      button: "Contact Location",
+    },
+    images: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
+  },
+  {
+    path: "images/Blood Compact Shrine/",
+    title: "Blood Compact Shrine",
+    description:
+      "This historic site commemorates the Blood Compact between Sikatuna and Miguel Lopez de Legaspi, symbolizing peace and friendship. The shrine marks a significant event in Philippine history, representing the first international treaty of friendship between Filipinos and Spaniards. Visitors can reflect on the importance of unity and cultural exchange while enjoying scenic views of the surrounding area.",
+    address: {
+      location: "J.P Inting Street, Tagbilaran City, 6300 Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3933.628466065994!2d123.87874129999999!3d9.6272231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa4dddf6a22e4d%3A0x1c874258d96a6677!2sBlood%20Compact%20Shrine!5e0!3m2!1sen!2sph!4v1732967284017!5m2!1sen!2sph",
+    },
+    fee: "Free",
+    contact: {
+      number: "+63 938 411 2014",
+      button: "Contact Location",
+    },
+    images: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
+  },
+  {
+    path: "images/Bohol Bee Farm/",
+    title: "Bohol Bee Farm",
+    description:
+      "Discover the wonders of beekeeping at Bohol Bee Farm, where you can enjoy scenic surroundings and savor local cuisine. Explore the farm's vibrant gardens, learn about sustainable farming practices, and taste delicious organic products made on-site. This eco-friendly destination offers a unique blend of nature, education, and culinary delights, making it a must-visit spot for a memorable experience.",
+    address: {
+      location: "Bohol Bee Farm, Dao, Dauis, Panglao Island, Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d503581.1718602869!2d123.21714668906247!3d9.5756128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa5300078621fb%3A0xe3f71fa920300a26!2sBohol%20Bee%20Farm%20Hotel!5e0!3m2!1sen!2sph!4v1732967358927!5m2!1sen!2sph",
+    },
+    fee: "PHP 500 (with Lunch)",
+    contact: {
+      number: "+63 938 502 2288",
+      button: "Contact Location",
+    },
+    images: ["1.JPG", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
+  },
+  {
+    path: "images/Chocolate Hills/",
+    title: "Chocolate Hills",
+    description:
+      "Witness the breathtaking, chocolate-brown hills formed by tidal movements, a natural wonder that captivates visitors. These unique geological formations, numbering over a thousand, transform from lush green to chocolate brown during the dry season. The Chocolate Hills are one of the Philippines' most iconic landscapes, offering panoramic views and unforgettable experiences for nature enthusiasts and photographers.",
+    address: {
+      location: "Carmen, Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15724.983370833097!2d124.13967560000002!3d9.8297078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa3da1a65d72f7%3A0xd89c856a3307dd67!2sChocolate%20Hills!5e0!3m2!1sen!2sph!4v1732967393951!5m2!1sen!2sph",
+    },
+    fee: "PHP 100 (view deck)",
+    contact: {
+      number: "+63 938 411 2014",
+      button: "Contact Location",
+    },
+    images: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"],
+  },
+  {
+    path: "images/Danao Adventure Park/",
+    title: "Danao Adventure Park",
+    description:
+      "Experience extreme adventures like the sky ride, plunge, and river trekking amidst nature at Danao Adventure Park. This thrilling destination offers adrenaline-pumping activities set against the backdrop of Bohol's stunning landscapes. Whether you're seeking a heart-racing plunge or a scenic river trek, the park provides an exhilarating escape for adventure seekers of all kinds.",
+    address: {
+      location: "Danao, Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.7128155743353!2d124.1789637!3d9.957834799999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa19485cdba799%3A0xfd87c4405e602f7b!2sDanao%20Adventure%20Park!5e0!3m2!1sen!2sph!4v1732967440041!5m2!1sen!2sph",
+    },
+    fee: "PHP 500 - PHP 2,000",
+    contact: {
+      number: "+63 938 510 0055",
+      button: "Contact Location",
+    },
+    images: ["1.jpg", "2.jpg", "3.JPG", "4.jpg", "5.jpg"],
+  },
+  {
+    path: "images/Loboc River/",
+    title: "Loboc River",
+    description:
+      "Experience a scenic river cruise along the Loboc River, where you can savor lunch at floating restaurants and marvel at stunning views of lush greens. The tranquil journey offers a unique perspective of Bohol's natural beauty, with traditional music and dance performances adding a cultural touch. Enjoy the serene ambiance and breathtaking landscapes as you glide through this picturesque river.",
+    address: {
+      location: "Poblacion Sawang, Loboc Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251680.7838996077!2d124.07524484999999!3d9.72260365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa40c60d6fcd0b%3A0x565db0188546a924!2sLoboc%20River!5e0!3m2!1sen!2sph!4v1732967479155!5m2!1sen!2sph",
+    },
+    fee: "PHP 500 - PHP 1,500",
+    contact: {
+      number: "+63 938 537 9141",
+      button: "Contact Location",
+    },
+    images: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"],
+  },
+  {
+    path: "images/Philippine Tarsier Foundation/",
+    title: "Philippine Tarsier Foundation",
+    description:
+      "Meet the smallest primates at the Philippine Tarsier Foundation, where you can learn about conservation efforts and support wildlife preservation. This sanctuary provides a safe haven for the endangered tarsiers, offering visitors an educational and immersive experience. By visiting, you contribute to the protection of these unique creatures and help promote biodiversity conservation.",
+    address: {
+      location: "Philippine Tarsier Foundation Inc., Corella Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.8858535800437!2d123.9526892!3d9.690782700000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa4925ec11288b%3A0xbf9bd933c18ea237!2sPhilippine%20Tarsier%20Sanctuary!5e0!3m2!1sen!2sph!4v1732967515797!5m2!1sen!2sph",
+    },
+    fee: "PHP 60",
+    contact: {
+      number: "+63 938 535 9275",
+      button: "Contact Location",
+    },
+    images: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
+  },
+  {
+    path: "images/Tigbao Hanging Bridge/",
+    title: "Tigbao Hanging Bridge",
+    description:
+      'Cross the steel and bamboo Tigbao Hanging Bridge, where you can explore souvenir shops and visit the "buko king" of Bohol. This thrilling attraction offers a unique and slightly adventurous experience as you walk across the swaying bridge. Enjoy the local delicacies and crafts available at the nearby shops, making it a memorable part of your Bohol journey.',
+    address: {
+      location: "Tigbao, Loboc Bohol",
+      button: "Get Direction",
+      site: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3933.173233358811!2d124.03624099678956!3d9.666235200000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aa45e4546f782f%3A0x46f6b31c76e8e876!2sTigbao%20Bridge!5e0!3m2!1sen!2sph!4v1732967616764!5m2!1sen!2sph",
+    },
+    fee: "Free",
+    contact: {
+      number: "+63 938 537 9141",
+      button: "Contact Location",
+    },
+    images: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"],
+  },
+];
+const screen = { xs: 2, md: 3, lg: 4, xl: 6 };
+let currentPage = 0;
+
+function renderPage(pageIndex) {
+  const spotItem = spot[pageIndex];
+  const paginationDiv = document.getElementById("pagination");
+  paginationDiv.innerHTML = `
+      <h3 class="text-center">${spotItem.title}</h3>
+      <div class="row justify-content-center">
+          ${spotItem.images
+            .map(
+              (img, index) => `<div class="col-6 col-md-4 col-lg-3 col-xl-${
+                12 / screen.xl
+              }">
+          <img src="${
+            spotItem.path + img
+          }" class="img-fluid mb-3 dynamicImage w-100" alt="${
+                spotItem.title
+              }" onclick="openModal('${spotItem.path + img}', ${index})"></div>`
+            )
+            .join("")}
+      </div>
+      <div class="description rounded">
+          <p>${spotItem.description}</p>
+      </div>
+      <div class="row align-items-center">
+          <div class="col-md-6">
+              <div class="embed-responsive embed-responsive-16by9 mb-3">
+                  <iframe class="embed-responsive-item" id="mapFrame" src="${
+                    spotItem.address.site
+                  }" width="100%" height="100%" allowfullscreen></iframe>
+              </div>
+          </div>
+          <div class="col-md-6 text-center">
+              <div>
+                  <p><strong>Address:</strong> ${spotItem.address.location}</p>
+                  <p><strong>Entrance Fee:</strong> ${spotItem.fee}</p>
+                  <p><strong>Contact Number:</strong> <a href="tel:${
+                    spotItem.contact.number
+                  }">${spotItem.contact.number}</a></p>
+                  <div class="d-flex justify-content-center">
+                      <button class="btn btn-secondary btn-small me-2" onclick="locateMap('${
+                        spotItem.title
+                      }')">${spotItem.address.button}</button>
+                      <button class="btn btn-secondary btn-small" onclick="contact('${
+                        spotItem.contact.number
+                      }')">${spotItem.contact.button}</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+  `;
+
+  // Make images clickable for opening the modal
+  document.querySelectorAll(".dynamicImage").forEach((image, index) => {
+    image.addEventListener("click", () => {
+      openModal(spotItem.path + spotItem.images[index]);
+    });
+  });
+}
+
+function updatePaginationControls() {
+  const controls = document.getElementById("pagination-controls");
+  const maxPages = spot.length;
+
+  controls.innerHTML = `
+      <li class="page-item my-1">
+          <a class="page-link" href="#anchorPage" aria-label="Previous" onclick="changePage(currentPage - 1)">
+              <span aria-hidden="true">&laquo;</span>
+          </a>
+      </li>
+  `;
+  for (let i = 0; i < Math.min(maxPages, spot.length); i++) {
+    controls.innerHTML += `<li class="page-item ${
+      i === currentPage ? "active" : ""
+    } mx-1 my-1"><a class="page-link" href="#anchorPage" onclick="changePage(${i})">${
+      i + 1
+    }</a></li>`;
+  }
+  controls.innerHTML += `
+      <li class="page-item my-1">
+          <a class="page-link" href="#anchorPage" aria-label="Next" onclick="changePage(currentPage + 1)">
+              <span aria-hidden="true">&raquo;</span>
+          </a>
+      </li>
+  `;
+}
+
+function changePage(pageIndex) {
+  if (pageIndex < 0 || pageIndex >= spot.length) return;
+  currentPage = pageIndex;
+  renderPage(currentPage);
+  updatePaginationControls();
+}
+
+function locateMap(title) {
+  const query = encodeURIComponent(title);
+  const url = `https://www.google.com/maps/search/?api=1&query=${query}`;
+  window.open(url, "_blank");
+}
+
+function contact(number) {
+  Swal.fire({
+    title: "Contact Information",
+    html: `
+          <p>Contact number: ${number}</p>
+          <button class="btn btn-primary" onclick="callNow('${number}')">CALL NOW</button>
+          <button class="btn btn-secondary" onclick="copyNumber('${number}')">COPY NUMBER</button>
+      `,
+    icon: "info",
+    showCloseButton: true,
+    showConfirmButton: false,
+  });
+}
+
+function openModal(imageSrc) {
+  const modalImage = document.getElementById("modalImage");
+  modalImage.style.maxWidth = "90vw";
+  modalImage.src = imageSrc;
+  const imageModal = new bootstrap.Modal(document.getElementById("imageModal"));
+  imageModal.show();
+
+  imageModal._element.addEventListener("hidden.bs.modal", () => {
+    document.body.style.opacity = 1; // Fix for opacity issue
+  });
+}
+
+function callNow(number) {
+  window.location.href = `tel:${number}`;
+}
+
+function copyNumber(number) {
+  navigator.clipboard.writeText(number).then(() => {
+    Swal.fire("Copied!", "Contact number copied to clipboard.", "success");
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderPage(currentPage);
+  updatePaginationControls();
+  window.addEventListener("resize", updatePaginationControls); // Update pagination on window resize
+});
